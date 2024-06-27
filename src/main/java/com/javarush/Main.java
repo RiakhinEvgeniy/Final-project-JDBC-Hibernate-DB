c317ec2435c55d51b97ea9b8a83daa583955413c
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-06-27 16:47:02 +0200

    Add methods in classes of entity. Add properties. Add configuration.

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -1,8 +1,67 @@
 package com.javarush;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javarush.dao.CityDAO;
+import com.javarush.dao.CountryDAO;
+import com.javarush.domain.City;
+import com.javarush.domain.Country;
+import com.javarush.domain.CountryLanguage;
+import io.lettuce.core.RedisClient;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
+import static java.util.Objects.nonNull;
+
 public class Main {
+
+    private final SessionFactory sessionFactory;
+    private final ObjectMapper objectMapper;
+    private final RedisClient redisClient;
+    private final CityDAO cityDAO;
+    private final CountryDAO countryDAO;
+
+    public Main() {
+        sessionFactory = prepareRelationalDB();
+        cityDAO = new CityDAO(sessionFactory);
+        countryDAO = new CountryDAO(sessionFactory);
+        objectMapper = new ObjectMapper();
+        redisClient = prepareRedisClient();
+    }
+
+    private RedisClient prepareRedisClient() {
+        return null;
+    }
+
+    private SessionFactory prepareRelationalDB() {
+        final SessionFactory sessionFactory;
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/world");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        properties.put(Environment.HBM2DDL_AUTO, "validate");
+        properties.put(Environment.STATEMENT_BATCH_SIZE, "100");
+
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(City.class)
+                .addAnnotatedClass(Country.class)
+                .addAnnotatedClass(CountryLanguage.class)
+                .addProperties(properties)
+                .buildSessionFactory();
+        return sessionFactory;
+    }
 
     public static void main(String[] args) {
         Main main = new Main();
+    }
+
+    private void shutdown() {
+        if (nonNull(sessionFactory)) sessionFactory.close();
+        if (nonNull(redisClient)) redisClient.shutdown();
     }
 }
---
+++ src/main/java/com/javarush/dao/CityDAO.java
@@ -0,0 +1,27 @@
+package com.javarush.dao;
+
+import com.javarush.domain.City;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class CityDAO {
+    private final SessionFactory sessionFactory;
+
+    public CityDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<City> getItems(int offset, int count) {
+        Query<City> cityQuery = sessionFactory.getCurrentSession().createQuery("select c from City c", City.class);
+        cityQuery.setFirstResult(offset);
+        cityQuery.setMaxResults(count);
+        return cityQuery.list();
+    }
+
+    public int getTotalCount() {
+        Query<Long> query = sessionFactory.getCurrentSession().createQuery("select count(c) from City c", Long.class);
+        return Math.toIntExact(query.uniqueResult());
+    }
+}
